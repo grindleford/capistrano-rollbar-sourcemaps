42be5e4c1af1f6d61ddff059b828b515b6ddfed8
Matt Atkins <353934+matkins@users.noreply.github.com>
2023-03-24 13:56:37 +0000

    Don't automatically hook into deploy

--- lib/capistrano/rollbar/sourcemaps.rb
+++ lib/capistrano/rollbar/sourcemaps.rb
@@ -1,10 +1,6 @@
 # Core tasks
 load File.expand_path('../../tasks/sourcemaps.rake', __FILE__)
 
-namespace :deploy do
-  after :set_current_revision, 'rollbar:sourcemaps:upload'
-end
-
 namespace :load do
   task :defaults do
     load "capistrano/rollbar/sourcemaps/defaults.rb"
